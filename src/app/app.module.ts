fd48b9dfaceb67da5680731c212257cbe8d006ea
CHAPMAN Peter <Peter.CHAPMAN@dhsoha.state.or.us>
2018-01-04 14:41:24 -0800

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { MatButtonModule, MatCheckboxModule } from '@angular/material';
 import { AppComponent } from './app.component';
-
+import 'hammerjs';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
+
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
